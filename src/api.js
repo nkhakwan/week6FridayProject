4a41218f099f82834e4a95a5cd894ee29ca655ad
nkhakwan <post_khan@yahoo.com>
2020-04-24 15:29:20 -0700

    commiting fix that removes unnecessary code from the js files

--- src/api.js
+++ src/api.js
@@ -1,22 +1,16 @@
 export class CurrencyExchange {
   async getCurrency(currency) {
     try {
-      console.log("logger has entered backend function call");
       
       let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
-      console.log("logger after the fetch");
       let jsonifiedResponse;
-      console.log(`logger ok and status ${response.ok} and ${response.status}`);
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
-        console.log(`logger has response ${jsonifiedResponse} and ${currency}`);
       } else {
         jsonifiedResponse = false;
-        console.log (`logger in a wrong area`);
       }
       return jsonifiedResponse;
     } catch(error) {
-      console.log (`logger in the catch area`);
       return false;
     }
   }
--- src/main.js
+++ src/main.js
@@ -2,17 +2,13 @@ import { CurrencyExchange } from './api.js';
 
 $(document).ready(function() {
   $('#exchangeRate').click(function() {
-    console.log(`logger entered click function`);
     let currencyType  =  parseInt($("#currency").val());
     const dollarAmount = parseInt($('#amount').val());
-    console.log(`does logger has currency ${currencyType} and amount ${dollarAmount}?`);
     $('#amount').val("");
     (async () => {
       let currencyExchange = new CurrencyExchange();
       const response = await currencyExchange.getCurrency(dollarAmount);
       const exchangeGot = currencyExchange.conversion(dollarAmount,currencyType,response.conversion_rates);
-      console.log(response.conversion_rates);
-      console.log(`logger's final data is here ${exchangeGot}`);
 
       getElements(exchangeGot);
     })();
